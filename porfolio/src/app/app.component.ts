878c850f87ccc5ed79fb1af020fe3665e6ca9475
abrew <abrew.abraham@innovaturelabs.com>
2022-06-24 17:50:37 +0530

    PDX

--- porfolio/src/app/app.component.ts
+++ porfolio/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent 
+{
   title = 'porfolio';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() 
+  {
+      this.setUpAnalytics();
+  }
+
+  setUpAnalytics() 
+  {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd) => 
+    {
+      gtag('config', 'G-BSC8004RT3',{page_path: event.urlAfterRedirects});
+    });
+  }
 }
